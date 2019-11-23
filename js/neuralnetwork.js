d7bfe3e0470df0d54c840cbf392e2d7c695e4a41
EngineersBox <toastcraft.info@gmail.com>
2019-11-23 15:43:12 +1100

    Moved neuralNetwork() instance to ML5NN wrapper for modular design

---
+++ js/neuralnetwork.js
@@ -0,0 +1,66 @@
+class ML5NN {
+
+    /**
+     * Create a new ML5 neural network instance
+     * 
+     * @param {Object} initOptions 
+     * @param {Object} trainOptions 
+     */
+    constructor(initOptions, trainOptions) {
+        this.initOptions = initOptions || {
+            inputs: ["x", "y"],
+            outputs: ["label"],
+            task: "classification",
+            debug: "true"
+        };
+        this.trainOptions = trainOptions || {
+            epochs: epoch_count,
+            learningRate: learning_rate
+        };
+        this.network = ml5.neuralNetwork(this.initOptions);
+    }
+
+    /**
+     * Add training data with (x, y) position and assosciated label
+     * 
+     * @param {Number} x_val 
+     * @param {Number} y_val 
+     * @param {*} target 
+     */
+    addData(x_val, y_val, target) {
+        let inputs = {
+            x: x_val,
+            y: y_val
+        }
+        let targets = {
+            label: target
+        }
+        this.network.addData(inputs, targets);
+    }
+
+    /**
+     * Train the network given the current data
+     */
+    train() {
+        this.network.normalizeData();
+        this.network.train(this.trainOptions, this.whileTraining, this.doneTraining);
+    }
+
+    /**
+     * Display the epoch and loss for each training epoch
+     * 
+     * @param {*} epoch 
+     * @param {*} loss 
+     */
+    whileTraining(epoch, loss) {
+        console.log(`[ML5 ${uid}]> Epoch ${epoch}, Loss: ${loss}`);
+    }
+
+    /**
+     * Display 'done' message when training epochs are complete
+     */
+    doneTraining() {
+        console.log(`[ML5 ${uid}]> Done Training.`);
+    }
+
+}
--- js/sketch.js
+++ js/sketch.js
@@ -2,20 +2,35 @@ let canvas;
 let CANV_W = 600;
 let CANV_H = 600;
 
-let NNID = UUID.generate();
-
-let data_input_type;
+let dType = {
+    MANUAL: true,
+    RANDOM: false
+};
+let data_input_type = dType.MANUAL;
 let data_val = 'A';
 
 /* ML5 Related */
 let model;
 let epoch_count;
 let learning_rate;
+let stage = "input";
+let uid;
+
+function toDType(val) {
+    switch(val) {
+        case "manual":
+            return dType.MANUAL;
+        case "randomised":
+            return dType.RANDOM;
+        default:
+            return dType.MANUAL;
+    }
+}
 
 function initConst() {
     epoch_count = parseInt(Utils.getElem("epoch_count").value);
     learning_rate = parseFloat(Utils.getElem("learning_rate").value);
-    data_input_type = Utils.getElem("t_data").value;
+    data_input_type = toDType(Utils.getElem("t_data").value);
 }
 
 function setup() {
@@ -24,54 +39,34 @@ function setup() {
     canvas.parent("sketch_view");
     background(255);
 
-    let options = {
-        inputs: ["x", "y"],
-        outputs: ["label"],
-        task: "classification",
-        debug: "true"
-    };
-    model = ml5.neuralNetwork(options);
-}
-
-function whileTraining(epoch, loss) {
-    console.log(`[ML5 ${NNID}]> Epoch ${epoch}, Loss: ${loss}`);
-}
-
-function doneTraining() {
-    console.log(`[ML5 ${NNID}]> Done Training.`);
+    model = new ML5NN();
+    uid = UUID.generate();
 }
 
 function keyPressed() {
     if (key == 't') {
-        let options = {
-            epochs: epoch_count,
-            learningRate: learning_rate
-        }
-        model.normalizeData();
-        model.train(options, whileTraining, doneTraining);
+        model.train();
     } else {
         data_val = key.toUpperCase();
     }
 }
 
 function mousePressed() {
-    if (data_input_type == "manual") {
-        let inputs = {
-            x: mouseX,
-            y: mouseY
+    if (stage == "input") {
+        if (data_input_type == dType.MANUAL) {
+            model.addData(mouseX, mouseY, data_val);
+            noFill();
+            stroke(0);
+            ellipse(mouseX, mouseY, 25);
+            textAlign(CENTER, CENTER);
+            text(data_val, mouseX, mouseY);
+        } else if (data_input_type = dType.RANDOM) {
+
         }
-        let target = {
-            label: data_val
-        }
-        model.addData(inputs, target);
-        noFill();
-        stroke(0);
-        ellipse(mouseX, mouseY, 25);
-        textAlign(CENTER, CENTER);
-        text(data_val, mouseX, mouseY);
     }
 }
 
 function draw() {
     initConst();
+    Utils.getElem("point_rand").disabled = data_input_type;
 }
